208d5520db0186a3b0e3163a261b00a1f1cd1d96
Radoslaw Lyson <radoslawlyson@gmail.com>
2024-01-15 11:23:05 +0100

    feat: Base project configuration

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import client, { Channel, Connection, ConsumeMessage } from 'amqplib';
+import mqConnection from './rabbit-mq-connection';
+
+const handleIncomingNotification = (msg: string) => {
+    try {
+  
+      const parsedMessage = JSON.parse(msg);
+  
+      console.log(`Received Notification`, parsedMessage);
+  
+      // Implement your own notification flow
+  
+    } catch (error) {
+      console.error(`Error While Parsing the message`);
+    }
+  };
+  
+  
+  const listen = async () => {
+  
+    await mqConnection.connect();
+  
+    await mqConnection.consume(handleIncomingNotification);
+  };
+  
+  listen();
---
+++ src/rabbit-mq-connection.ts
@@ -0,0 +1,52 @@
+import client, { Channel, Connection } from "amqplib";
+type HandlerCB = (msg: string) => any;
+
+class RabbitMQConnection {
+    connection!: Connection;
+    channel!: Channel;
+    private connected!: Boolean;
+
+    async connect() {
+        if(this.connected && this.channel) return;
+
+        try {
+            console.log('Connection to RabbitMQ');
+
+            this.connection = await client.connect(
+                'amqp://localhost:5672/'
+            );
+
+            console.log('RabbitMQ connection is ready');
+
+            this.channel = await this.connection.createChannel();
+
+            console.log('Created channel successfully');
+
+            this.connected = true;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async consume(handleIncomingNotification: HandlerCB) {
+        await this.channel.assertQueue('test', { durable: true });
+
+        this.channel.consume('test', (msg) => {
+            {
+                if (!msg) {
+                  return console.error(`Invalid incoming message`);
+                }
+                handleIncomingNotification(msg?.content?.toString());
+                this.channel.ack(msg);
+              }
+            },
+            {
+              noAck: false,
+            }
+
+        )
+    }
+}
+
+const mqConnection = new RabbitMQConnection();
+export default mqConnection
